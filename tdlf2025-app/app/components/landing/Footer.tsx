bb3eef1a19a712ee8aa6aacc257d4fda04d9cab5
francisco-solis99 <franciscosoliscss@gmail.com>
2025-03-07 21:36:33 -0600

    style: add responsive to the footer section

--- tdlf2025-app/app/components/landing/Footer.tsx
+++ tdlf2025-app/app/components/landing/Footer.tsx
@@ -4,7 +4,7 @@ import Container from "./Container";
 export default function Footer() {
   return (
     <footer className="py-10">
-      <Container className="flex justify-between items-center">
+      <Container className="flex flex-col gap-y-5 md:flex-row md:justify-between items-center">
         <div className="flex gap-x-3.5 items-center">
           <Image
             src={"/logo.png"}
@@ -14,11 +14,11 @@ export default function Footer() {
             className="max-w-[80px]"
           />
           <div>
-            <h4 className="text-lg font-bold">TDLF-2025</h4>
-            <span>23 de Marzo 2025</span>
+            <h4 className="text-base sm:text-lg font-bold">TDLF-2025</h4>
+            <span className="text-sm xs:text-base">23 de Marzo 2025</span>
           </div>
         </div>
-        <div className="text-sm text-right">
+        <div className="text-xs xs:text-sm text-center md:text-right">
           <p>
             Si quieres contactar escribe al numero{" "}
             <a
