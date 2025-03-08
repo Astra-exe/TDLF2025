b1448241d123a3211d096f0006aff347da44ee35
francisco-solis99 <franciscosoliscss@gmail.com>
2025-03-07 21:34:49 -0600

    style: add responsive to the need to know section

--- tdlf2025-app/app/components/landing/NeedToKnow.tsx
+++ tdlf2025-app/app/components/landing/NeedToKnow.tsx
@@ -5,12 +5,12 @@ export default function NeedToKnow() {
   return (
     <Container className="mt-32">
       <section className="text-white">
-        <h3 className="text-6xl font-bold text-center">
+        <h3 className="text-[40px] font-bold text-center sm:text-5xl lg:text-6xl">
           Lo que necesitas saber
         </h3>
-        <div className="mt-12 grid grid-cols-2 md:grid-cols-10 md:grid-rows-[repeat(3,auto)] gap-2 lg:gap-3">
-          <BentoCard className="relative col-span-5 flex justify-between items-center">
-            <h4 className="flex flex-col gap-y-2 font-bold text-5xl">
+        <div className="mt-12 grid grid-cols-10 md:grid-rows-[repeat(3,auto)] gap-2 lg:gap-3">
+          <BentoCard className="relative col-span-full flex flex-col gap-3 justify-between items-center lg:col-span-5 xs:gap-0 xs:flex-row">
+            <h4 className="flex flex-col gap-y-2 font-bold text-4xl lg:text-5xl">
               <span>Dos</span>
               <span>Categorias</span>
             </h4>
@@ -19,18 +19,18 @@ export default function NeedToKnow() {
               alt="Raqueta de Frontenis"
               width={263}
               height={263}
-              className="max-w-[180px]"
+              className="w-full h-full object-cover max-w-[140px] lg:max-w-[180px]"
             />
           </BentoCard>
-          <BentoCard className="col-span-5 relative">
+          <BentoCard className="col-span-full relative lg:col-span-5">
             <Image
               src={"/categoria.png"}
               alt="Raqueta tomada por un jugador"
               width={263}
               height={263}
-              className="absolute left-0 top-0"
+              className="w-full h-full object-cover max-w-[180px] sm:max-w-[220px] absolute -left-10 top-0 xl:max-w-none xl:left-0"
             />
-            <div className="h-full flex flex-col justify-between">
+            <div className="h-full flex flex-col justify-between items-end gap-y-4">
               <div className="relative flex items-center gap-x-2 justify-center">
                 <span className="text-primary">
                   <svg
@@ -45,7 +45,9 @@ export default function NeedToKnow() {
                     />
                   </svg>
                 </span>
-                <span className="text-5xl font-medium">Libre</span>
+                <span className="font-medium text-3xl sm:text-4xl lg:text-5xl">
+                  Libre
+                </span>
               </div>
               <div className="relative flex items-center gap-x-2 justify-end">
                 <span className="text-primary">
@@ -61,21 +63,25 @@ export default function NeedToKnow() {
                     />
                   </svg>
                 </span>
-                <span className="text-5xl font-medium">50 y más</span>
+                <span className="font-medium text-3xl sm:text-4xl lg:text-5xl">
+                  50 y más
+                </span>
               </div>
             </div>
           </BentoCard>
-          <BentoCard className="col-span-6 relative">
+          <BentoCard className="flex flex-col-reverse items-center pb-0 col-span-full row-start-1 relative lg:col-span-6 lg:row-start-2 md:pb-10">
             <Image
               src={"/inscripcion.png"}
               alt="Jugadores jugando"
               width={240}
               height={315}
-              className="max-w-[180px] absolute bottom-0 right-0"
+              className="max-w-[180px] md:absolute md:bottom-0 md:right-0"
             />
-            <div className="w-[70%]">
-              <h4 className="text-5xl font-bold relative">Inscripción</h4>
-              <ul className="mt-5 text-xl list-disc list-inside space-y-3">
+            <div className="xs:w-[70%]">
+              <h4 className="font-bold relative text-4xl lg:text-5xl">
+                Inscripción
+              </h4>
+              <ul className="mt-5 list-disc list-inside space-y-3 text-lg lg:text-xl">
                 <li>
                   El costo de la inscripción por persona es de{" "}
                   <strong className="text-primary">$350 MXN</strong>
@@ -84,9 +90,9 @@ export default function NeedToKnow() {
               </ul>
             </div>
           </BentoCard>
-          <BentoCard className="col-span-4 row-span-2">
-            <h4 className="text-5xl font-bold">Grupos y Parejas</h4>
-            <ul className="mt-7 mb-5 list-disc space-y-3 text-lg">
+          <BentoCard className="col-span-full md:col-span-6 row-span-2 lg:col-span-4 lg:row-start-2">
+            <h4 className="font-bold text-4xl lg:text-5xl">Grupos y Parejas</h4>
+            <ul className="mt-7 mb-5 list-disc space-y-3 lg:text-lg">
               <li>
                 El número de parejas proyectadas (esto puede cambiar) en la
                 categoria libre será de 24. Teniendo 6 grupos con 4 parejas cada
@@ -106,9 +112,9 @@ export default function NeedToKnow() {
               className="max-w-[180px] mx-auto"
             />
           </BentoCard>
-          <BentoCard className="col-span-6">
-            <h4 className="text-5xl font-bold">Cancha y Pelotas</h4>
-            <ul className="mt-7 list-disc space-y-3 list-inside text-lg">
+          <BentoCard className="col-span-full md:col-span-4 lg:col-span-6">
+            <h4 className="font-bold text-4xl lg:text-5xl">Cancha y Pelotas</h4>
+            <ul className="mt-7 list-disc space-y-3 list-inside lg:text-lg">
               <li>
                 El número de canchas destinadas para los partidos serán 3.
               </li>
