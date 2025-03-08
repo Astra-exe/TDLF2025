9b3ada82417538bbac084f753df8365bd466f047
francisco-solis99 <franciscosoliscss@gmail.com>
2025-03-07 21:30:07 -0600

    style: add responsive to the hero and info section

--- tdlf2025-app/app/components/landing/Hero.tsx
+++ tdlf2025-app/app/components/landing/Hero.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Hero() {
   return (
-    <main className="relative -mt-[100px] flex flex-col items-center overflow-hidden pt-[23vh] pb-[25vh]">
-      <div className="-z-10 absolute  inset-0 grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-2 bg-linear-180 from-black via-accent to-primary [&>div]:w-full [&>div]:aspect-square [&>div]:rounded-md [&>div]:bg-primary/10">
+    <main className="px-8 sm:px-0 relative -mt-[100px] flex flex-col items-center overflow-hidden pt-[20vh] pb-[18vh] sm:pt-[23vh] sm:pb-[25vh]">
+      <div className="-z-10 absolute  inset-0 grid grid-cols-[repeat(auto-fit,minmax(100px,1fr))] sm:grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-2 bg-linear-180 from-black via-accent to-primary [&>div]:w-full [&>div]:aspect-square [&>div]:rounded-md [&>div]:bg-primary/10">
         <div></div>
         <div></div>
         <div></div>
@@ -41,23 +41,25 @@ export default function Hero() {
           width={684}
           height={684}
           alt="Logo TDLF-2025"
-          className="max-w-[280px] blur-lg absolute top-0 left-0"
+          className="max-w-[190px] xs:max-w-[220px] sm:max-w-[280px] blur-lg absolute top-0 left-0"
         />
         <Image
           src={"/logo.png"}
           width={684}
           height={684}
           alt="Logo TDLF-2025"
-          className="max-w-[280px] relative"
+          className="max-w-[190px] xs:max-w-[220px] sm:max-w-[280px] relative"
         />
       </div>
-      <div className="mt-12">
-        <h1 className="flex flex-col gap-y-7 text-7xl text-center leading-32 ">
+      <div className="mt-8 sm:mt-12">
+        <h1 className="flex flex-col gap-y-7 text-[40px] text-center leading-14 xs:leading-20 xs:text-5xl sm:leading-24 lg:leading-32 sm:text-6xl lg:text-7xl">
           <span className="font-nosifer">
             Torneo de las{" "}
             <strong className="text-primary saturate-200">Fresas</strong>
           </span>
-          <span className="text-6xl font-medium">Tercera edición - 2025</span>
+          <span className="text-3xl font-medium xs:text-4xl sm:text-5xl lg:text-6xl">
+            Tercera edición - 2025
+          </span>
         </h1>
       </div>
     </main>
--- tdlf2025-app/app/components/landing/Info.tsx
+++ tdlf2025-app/app/components/landing/Info.tsx
@@ -4,11 +4,11 @@ export default function Info() {
   return (
     <section className="bg-primary pt-24 pb-32 text-black">
       <Container>
-        <h2 className="text-center text-6xl">
+        <h2 className="text-center text-3xl xs:text-4xl sm:text-5xl lg:text-6lx">
           El dia que Irapuato se vuelve sede del{" "}
           <strong>frontenis de más alto nivel</strong>
         </h2>
-        <div className="mt-9 flex justify-center gap-8">
+        <div className="mt-9 flex flex-col justify-center gap-8 sm:flex-row">
           <article className="grid gap-5 border-2 px-8 py-16">
             <div className="inline-flex items-center gap-x-1.5">
               <svg
@@ -22,10 +22,12 @@ export default function Info() {
                   d="M19 19H5V8h14m-3-7v2H8V1H6v2H5c-1.11 0-2 .89-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V5a2 2 0 0 0-2-2h-1V1m-1 11h-5v5h5z"
                 />
               </svg>
-              <span className="text-2xl">¿Cuándo?</span>
+              <span className="text-xl lg:text-2xl">¿Cuándo?</span>
             </div>
-            <h3 className="text-5xl">23 de Marzo 2025</h3>
-            <span className="text-2xl">Domingo</span>
+            <h3 className="font-medium text-4xl lg:text-5xl">
+              23 de Marzo 2025
+            </h3>
+            <span className="text-xl lg:text-2xl">Domingo</span>
           </article>
           <article className="grid gap-5 border-2 px-8 py-16">
             <div className="inline-flex items-center gap-x-1.5">
@@ -40,12 +42,14 @@ export default function Info() {
                   d="M12 3a7 7 0 0 0-7 7c0 2.862 1.782 5.623 3.738 7.762A26 26 0 0 0 12 20.758q.262-.201.615-.49a26 26 0 0 0 2.647-2.504C17.218 15.623 19 12.863 19 10a7 7 0 0 0-7-7m0 20.214l-.567-.39l-.003-.002l-.006-.005l-.02-.014l-.075-.053l-.27-.197a28 28 0 0 1-3.797-3.44C5.218 16.875 3 13.636 3 9.999a9 9 0 0 1 18 0c0 3.637-2.218 6.877-4.262 9.112a28 28 0 0 1-3.796 3.44a17 17 0 0 1-.345.251l-.021.014l-.006.005l-.002.001zM12 8a2 2 0 1 0 0 4a2 2 0 0 0 0-4m-4 2a4 4 0 1 1 8 0a4 4 0 0 1-8 0"
                 />
               </svg>
-              <span className="text-2xl">¿Dónde?</span>
+              <span className="text-xl lg:text-2xl">¿Dónde?</span>
             </div>
-            <h3 className="text-5xl">Deportiva Norte</h3>
+            <h3 className="font-medium text-4xl lg:text-5xl">
+              Deportiva Norte
+            </h3>
             <a
               href="https://www.google.com/maps/place/Unidad+Deportiva+Mario+Vazquez+Ra%C3%B1a/@20.6944313,-101.3764419,15z/data=!4m6!3m5!1s0x842c7f9746cdae27:0x6d070463c2cb0140!8m2!3d20.6944313!4d-101.3764419!16s%2Fg%2F119tgd4jz?entry=ttu&g_ep=EgoyMDI1MDMwMi4wIKXMDSoASAFQAw%3D%3D"
-              className="inline-flex gap-x-0.5 items-center text-2xl underline font-medium hover:opacity-80 transition-opacity group"
+              className="inline-flex gap-x-0.5 items-center text-xl lg:text-2xl underline font-medium hover:opacity-80 transition-opacity group"
               target="_blank"
               rel="nofollow"
             >
