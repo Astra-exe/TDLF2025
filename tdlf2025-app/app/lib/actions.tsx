fae935a5e88314494678e491c0bc51abd0832f92
francisco-solis99 <franciscosoliscss@gmail.com>
2025-03-15 11:51:46 -0600

    feat: define teh actions to create players, groups signIn and signOut

--- tdlf2025-app/app/lib/actions.tsx
+++ tdlf2025-app/app/lib/actions.tsx
@@ -2,10 +2,20 @@
 
 import { signIn, signOut } from "@/auth";
 import { AuthError } from "next-auth";
+import type { Player, Pair } from "./definitions";
+import { auth } from "@/auth";
+
+type Credentials = {
+  username: string;
+  password: string;
+};
+
+type NewPlayer = Omit<Player, "id" | "is_active">;
+type NewPair = Omit<Pair, "id" | "is_eliminated">;
 
 export async function authenticate(
   prevState: string | undefined,
-  formData: FormData
+  formData: Credentials
 ) {
   try {
     await signIn("credentials", formData);
@@ -15,7 +25,7 @@ export async function authenticate(
         case "CredentialsSignin":
           return "Credenciales Invalidas.";
         default:
-          return "Something went wrong.";
+          return "Ocurrio un error, intenta más tarde";
       }
     }
     throw error;
@@ -24,4 +34,67 @@ export async function authenticate(
 
 export async function logOut() {
   await signOut({ redirectTo: "/" });
+}
+
+export async function createPlayer(playerData: NewPlayer) {
+  console.log({ playerData });
+  try {
+    const session = await auth();
+    // console.log({ session });
+    if (!session?.user) {
+      throw new Error("No autorizado");
+    }
+    console.log("autorizado create player");
+    // fetch to create it
+    // const url = `${process.env.NEXT_PUBLIC_API_URL}/v1/players`;
+    // const response = await fetch(url, {
+    //   method: "POST",
+    //   body: JSON.stringify(playerData),
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //     X_API_KEY: session.apiKey,
+    //   },
+    // });
+    // const data = await response.json();
+
+    // if(response.ok) {
+    //   return data
+    // }
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function createPair(pairData: NewPair) {
+  console.log({ pairData });
+  try {
+    const session = await auth();
+    // console.log({ session });
+    if (!session?.user) {
+      throw new Error("No autorizado");
+    }
+    console.log("autorizado create pair");
+    // fetch to create it
+    // const url = `${process.env.NEXT_PUBLIC_API_URL}/v1/pairs`;
+    // const response = await fetch(url, {
+    //   method: "POST",
+    //   body: JSON.stringify(pairData),
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //     X_API_KEY: session.apiKey,
+    //   },
+    // });
+
+    // if(!response.ok) {
+    // const errorData = await response.json();
+    // throw new Error(errorData?.message || "Error al crear la pareja");
+    // }
+    // const data = await response.json()
+    // return data
+  } catch (error) {
+    if (error instanceof Error) {
+      throw error;
+    }
+    throw new Error("Error desconocido al crear la pareja");
+  }
 }
