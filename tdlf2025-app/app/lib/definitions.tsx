14efe9701e1e744d4287d19b90bbed12caf33376
francisco-solis99 <franciscosoliscss@gmail.com>
2025-03-15 11:44:37 -0600

    chore: add type definitions

--- tdlf2025-app/app/lib/definitions.tsx
+++ tdlf2025-app/app/lib/definitions.tsx
@@ -3,3 +3,27 @@ export type User = {
   username: string;
   apiKey: string;
 };
+
+export type Player = {
+  id: string;
+  fullname: string;
+  city: string;
+  weight: number;
+  height: number;
+  age: number;
+  experience: number;
+  is_active: number;
+};
+
+export type Pair = {
+  id: string;
+  players: [string, string];
+  is_eliminated: number;
+  registration_category_id: string;
+};
+
+export type Category = {
+  id: string;
+  name: string;
+  description: string;
+};
