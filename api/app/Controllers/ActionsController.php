da1db5b9c7c29b5391acf91a8fb2f30cc548e8dd
Ricardo García Jiménez <ricardogj08@riseup.net>
2025-03-16 20:19:36 -0600

    feat: Generates groups names into action for sets randomize pairs into groups
    Signed-off-by: Ricardo García Jiménez <ricardogj08@riseup.net>

--- api/app/Controllers/ActionsController.php
+++ api/app/Controllers/ActionsController.php
@@ -21,29 +21,34 @@ class ActionsController extends BaseController
     public function randomizeGroupsPairsMatches(): void
     {
         $settings = [
-            'open' => ['max_pairs' => 4],
-            'seniors' => ['max_pairs' => 3],
+            'open' => ['max_pairs' => 4, 'max_groups' => 27],
+            'seniors' => ['max_pairs' => 3, 'max_groups' => 27],
         ];
 
-        $registrationCategories = new RegistrationCategoryModel;
-
         // Obtiene la información de todas las "categorías de inscripción".
-        $categories = $registrationCategories->select('id', 'name')
+        $categories = (new RegistrationCategoryModel)->select('id', 'name')
             ->in('name', array_keys($settings))
             ->findAll();
 
+        // Modelos.
         $pair = new PairModel;
         $group = new GroupModel;
         $groupPairPivot = new GroupPairPivotModel;
 
         $column = 'registration_category_id';
 
+        // Obtiene el año actual.
+        $year = Date::getCurrentYear();
+
         // Formato del nombre de los grupos.
         // Nombre: A_2025, B_2025, C_2025, ...
         // Descripción: A, B, C, ...
         foreach ($categories as $category) {
             // Obtiene el número de "parejas" de la "categoría".
-            $pair->select('COUNT(*) AS _count')->eq($column, $category->id)->find();
+            $pair->select('COUNT(*) AS _count')
+                ->eq($column, $category->id)
+                ->eq('is_active', 1)
+                ->find();
 
             // Obtiene el número máximo de "parejas" por "grupo".
             $maxPairs = $settings[$category->name]['max_pairs'];
@@ -56,15 +61,30 @@ class ActionsController extends BaseController
                 $this->respondDependecyError('The registration category of pairs players does not complete the groups');
             }
 
+            // Obtiene el número máximo de "grupos" permitidos.
+            $maxGroups = $settings[$category->name]['max_groups'];
+
+            // Comprueba el número máximo de "grupos" permitidos.
+            if ($numGroups > $maxGroups) {
+                $this->respondDependecyError('The registration category of pairs players exceeds the number of allowed groups');
+            }
+
             // Genera los nombres de los "grupos".
-            $groupsNames = [];
+            $groupsNames = range('A', chr(64 + $numGroups));
+
+            // Agrega la letra "Ñ" a los nombres de los "grupos".
+            if ($numGroups > 14) {
+                array_splice($groupsNames, 14, 0, 'Ñ');
+                array_pop($groupsNames);
+            }
 
             // Obtiene las "parejas" de la "categoría" y lo ordena de manera aleatoria.
-            $pairs = $pair->reset()->select('id')->eq($column, $category->id)->findAll();
-            shuffle($pairs);
+            $pairs = $pair->reset()->select('id')
+                ->eq($column, $category->id)
+                ->eq('is_active', 1)
+                ->findAll();
 
-            // Obtiene el año actual.
-            $year = Date::getCurrentYear();
+            shuffle($pairs);
 
             // Segmenta las "parejas" en el número total de "grupos".
             foreach (array_chunk($pairs, $numGroups) as $key => $segment) {
@@ -74,6 +94,7 @@ class ActionsController extends BaseController
                     'name' => sprintf('%s_%u', $groupsNames[$key], $year),
                     'description' => $groupsNames[$key],
                 ]);
+
                 $group->insert();
 
                 // Consulta la información del "grupo" registrado.
