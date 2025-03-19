15f98c461180f2841fde38e3a30228d586e78792
francisco-solis99 <franciscosoliscss@gmail.com>
2025-03-18 20:14:47 -0600

    refactor: the request data needed to create a new pair

--- tdlf2025-app/app/dashboard/parejas/crear/page.tsx
+++ tdlf2025-app/app/dashboard/parejas/crear/page.tsx
@@ -1,7 +1,19 @@
 import AppBreadcrumb from "@/app/components/AppBreadcrumb";
 import PairsForm from "@/app/components/PairsForm";
+import { Player } from "@/app/lib/definitions";
+import { fetchAllPlayers, fetchCategories } from "@/app/lib/data";
+import { auth } from "@/auth";
 
-export default function CreatePairsPage() {
+export default async function CreatePairsPage() {
+  const session = await auth();
+  if (!session?.user || !session?.user?.apiKey) {
+    throw new Error("No autorizado");
+  }
+  const apiKey = session.user.apiKey;
+
+  const playersData: Player[] = await fetchAllPlayers(apiKey);
+  const categoriesData = await fetchCategories(apiKey);
+
   return (
     <div className="w-full pt-8 pb-12">
       <div className="w-[80%] mx-auto">
@@ -21,7 +33,10 @@ export default function CreatePairsPage() {
         <h1 className="text-3xl md:text-4xl font-bold text-center mt-5 mb-7">
           Crear Parejas
         </h1>
-        <PairsForm />
+        <PairsForm
+          playersList={playersData}
+          categoriesList={categoriesData.data}
+        />
       </div>
     </div>
   );
