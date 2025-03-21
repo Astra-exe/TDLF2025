8dcf2302f68e689d25fb8175ddf3ecbf3988e5b6
francisco-solis99 <franciscosoliscss@gmail.com>
2025-03-21 09:11:55 -0600

    feat: add the pairs page with public columns of pairs in the table without the actions

--- tdlf2025-app/app/components/PairsCols.tsx
+++ tdlf2025-app/app/components/PairsCols.tsx
@@ -1,8 +1,10 @@
 "use client";
 import { ColumnDef } from "@tanstack/react-table";
-import { Button } from "@/app/components/ui/button";
-import { Edit, Trash } from "lucide-react";
 import Link from "next/link";
+import {
+  UpdatePair,
+  DeletePair,
+} from "@/app/components/dashboard/pairs/buttons";
 
 export type PairRow = {
   id: string;
@@ -16,6 +18,38 @@ export type PairRow = {
   };
   category: "Libre" | "Seniors";
 };
+
+export const columnsPublic: ColumnDef<PairRow>[] = [
+  {
+    accessorKey: "player1",
+    header: "Jugador 1",
+    cell: ({ row }) => (
+      <Link
+        href={`/jugador/${row.original.player1.id}`}
+        className="hover:underline transition-all"
+      >
+        {row.original.player1.name}
+      </Link>
+    ),
+  },
+  {
+    accessorKey: "player2",
+    header: "Jugador 2",
+    cell: ({ row }) => (
+      <Link
+        href={`/jugador/${row.original.player2.id}`}
+        className="hover:underline transition-all"
+      >
+        {row.original.player2.name}
+      </Link>
+    ),
+  },
+  {
+    accessorKey: "category",
+    header: "Categoria",
+    cell: ({ row }) => <span>{row.getValue("category")}</span>,
+  },
+];
 
 export const columns: ColumnDef<PairRow>[] = [
   {
@@ -54,26 +88,10 @@ export const columns: ColumnDef<PairRow>[] = [
       const pair = row.original;
       return (
         <div className="flex space-x-2">
-          <Button variant="outline" size="sm" onClick={() => handleEdit(pair)}>
-            <Edit className="w-4 h-4" />
-          </Button>
-          <Button
-            variant="outline"
-            size="sm"
-            onClick={() => handleDelete(pair)}
-          >
-            <Trash className="w-4 h-4" />
-          </Button>
+          <UpdatePair id={pair.id} />
+          <DeletePair id={pair.id} />
         </div>
       );
     },
   },
 ];
-
-const handleEdit = (data: PairRow) => {
-  alert(`Editing ${data.player1} & ${data.player2}`);
-};
-
-const handleDelete = (data: PairRow) => {
-  alert(`Deleting ${data.player1} & ${data.player2}`);
-};
