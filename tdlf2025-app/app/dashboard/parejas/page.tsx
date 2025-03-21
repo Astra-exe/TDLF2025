7e1c868d538c0837f3b4fd2b1a5b1fefc611acb8
francisco-solis99 <franciscosoliscss@gmail.com>
2025-03-21 09:19:58 -0600

    refactor: auth code to get the api key and pass to the data file

--- tdlf2025-app/app/dashboard/parejas/page.tsx
+++ tdlf2025-app/app/dashboard/parejas/page.tsx
@@ -2,8 +2,7 @@ import AppBreadcrumb from "@/app/components/AppBreadcrumb";
 import { ButtonCreatePair } from "@/app/components/ButtonsPairs";
 import { columns, PairRow } from "@/app/components/PairsCols";
 import TablePairs from "@/app/components/TablePairs";
-import { fetchPairs, fetchPairById } from "@/app/lib/data";
-import type { PairsPlayers } from "@/app/lib/definitions";
+import { fetchTablePairData } from "@/app/lib/data";
 import { auth } from "@/auth";
 
 export default async function PairsPage() {
@@ -12,28 +11,7 @@ export default async function PairsPage() {
     throw new Error("No autorizado");
   }
   const apiKey = session.user?.apiKey;
-
-  const { data: dataPairs } = await fetchPairs(apiKey);
-  const tableDataPromises = dataPairs.map(async (pairData: PairsPlayers[]) => {
-    const [player1Info, player2Info] = pairData;
-    const idPair = player1Info.relationship.pair_id;
-    const pairInfo = await fetchPairById({ idPair, apiKey });
-
-    return {
-      id: idPair,
-      player1: {
-        id: player1Info?.player?.id,
-        name: player1Info?.player?.fullname ?? "No reconocido",
-      },
-      player2: {
-        id: player2Info?.player?.id,
-        name: player2Info?.player?.fullname ?? "No reconocido",
-      },
-      category:
-        pairInfo?.data?.registration_category?.description ?? "Sin categoria",
-    };
-  });
-  const tableData: PairRow[] = await Promise.all(tableDataPromises);
+  const tableData: PairRow[] = await fetchTablePairData(apiKey);
 
   return (
     <div className="w-full pt-8 pb-12">
