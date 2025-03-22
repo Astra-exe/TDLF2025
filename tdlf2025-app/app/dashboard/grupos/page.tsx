8a29f13ca1fb968b8ba2d3d204bc46c3fc5e6412
francisco-solis99 <franciscosoliscss@gmail.com>
2025-03-22 01:33:57 -0600

    feat: add the principal page fpr groups and matches

--- tdlf2025-app/app/dashboard/grupos/page.tsx
+++ tdlf2025-app/app/dashboard/grupos/page.tsx
@@ -1,24 +1,24 @@
+import Link from "next/link";
 import AppBreadcrumb from "@/app/components/AppBreadcrumb";
-import Group from "@/app/components/Group";
+import { Category } from "@/app/lib/definitions";
+import { auth } from "@/auth";
+import { LinkIcon } from "lucide-react";
+import { fetchCategories, fetchGroups } from "@/app/lib/data";
+import GenerateGroupsPlayers from "@/app/components/GenerateGroupsMatches";
 
-export default function GroupsPage() {
-  const listPairs = [
-    {
-      id: "1",
-      player1: "Roger Federer",
-      player2: "Rafa Nadal",
-    },
-    {
-      id: "2",
-      player1: "Raul Fonseca",
-      player2: "Ben Shelton",
-    },
-    {
-      id: "3",
-      player1: "Sasha Zverev",
-      player2: "Daniil Medvedev",
-    },
-  ];
+export default async function GroupsPage() {
+  const session = await auth();
+  if (!session?.user || !session?.user?.apiKey) {
+    throw new Error("No autorizado");
+  }
+  const apiKey = session.user?.apiKey;
+  const { pagination } = await fetchGroups(apiKey);
+  const existGruops = pagination.count > 0;
+  let categories = null;
+  if (existGruops) {
+    const { data } = await fetchCategories(apiKey);
+    categories = data;
+  }
 
   return (
     <div className="w-full pt-8 pb-12">
@@ -35,14 +35,24 @@ export default function GroupsPage() {
         <h1 className="text-3xl md:text-4xl font-bold text-center mb-7">
           Grupos
         </h1>
-        <section className="grid grid-cols-2 md:grid-cols-3 gap-5">
-          <Group name="A" listPairs={listPairs} />
-          <Group name="B" listPairs={listPairs} />
-          <Group name="C" listPairs={listPairs} />
-          <Group name="D" listPairs={listPairs} />
-          <Group name="F" listPairs={listPairs} />
+        <section className="flex justify-center min-h-[300px] items-center gap-4 md:gap-8">
+          {!existGruops ? (
+            <GenerateGroupsPlayers />
+          ) : (
+            categories.map((category: Category) => {
+              return (
+                <Link
+                  key={category.id}
+                  href={`/grupos/${category.id}`}
+                  className="flex flex-col items-center gap-y-1.5 px-4 py-2 border bg-primary text-white font-bold hover:opacity-80 transition-colors"
+                >
+                  Ver Grupos Categoria {category.description}
+                  <LinkIcon />
+                </Link>
+              );
+            })
+          )}
         </section>
-        {/* Table or flipflop of Groups */}
       </div>
     </div>
   );
--- tdlf2025-app/app/dashboard/partidos/page.tsx
+++ tdlf2025-app/app/dashboard/partidos/page.tsx
@@ -1,37 +1,23 @@
 import AppBreadcrumb from "@/app/components/AppBreadcrumb";
-import Match from "@/app/components/Match";
+import GenerateGroupsPlayers from "@/app/components/GenerateGroupsMatches";
+import { fetchCategories, fetchGroups } from "@/app/lib/data";
+import { Category } from "@/app/lib/definitions";
+import { auth } from "@/auth";
+import Link from "next/link";
 
-export default function MatchesPage() {
-  const listMatches = [
-    {
-      id: "1",
-      player1Pair1: "Roger Federer",
-      player2Pair1: "Rafa Nadal",
-      player1Pair2: "Raul Fonseca",
-      player2Pair2: "Ben Shelton",
-      pointsPair1: 10,
-      pointsPair2: 8,
-    },
-    {
-      id: "2",
-      player1Pair1: "Roger Federer",
-      player2Pair1: "Rafa Nadal",
-      player1Pair2: "Raul Fonseca",
-      player2Pair2: "Ben Shelton",
-      pointsPair1: 10,
-      pointsPair2: 5,
-    },
-    {
-      id: "3",
-      player1Pair1: "Roger Federer",
-      player2Pair1: "Rafa Nadal",
-      player1Pair2: "Raul Fonseca",
-      player2Pair2: "Ben Shelton",
-      pointsPair1: 4,
-      pointsPair2: 10,
-    },
-  ];
-
+export default async function MatchesPage() {
+  const session = await auth();
+  if (!session?.user || !session?.user?.apiKey) {
+    throw new Error("No autorizado");
+  }
+  const apiKey = session.user?.apiKey;
+  const { pagination } = await fetchGroups(apiKey);
+  const existGruops = pagination.count > 0;
+  let categories = null;
+  if (existGruops) {
+    const { data } = await fetchCategories(apiKey);
+    categories = data;
+  }
   return (
     <div className="w-full pt-8 pb-12">
       <div className="w-[80%] mx-auto">
@@ -47,21 +33,23 @@ export default function MatchesPage() {
         <h1 className="text-3xl md:text-4xl font-bold text-center mb-7">
           Partidos
         </h1>
-        {/* Searcher */}
-        <section className="grid grid-cols-2 md:grid-cols-3 gap-5">
-          {listMatches.map((match) => {
-            return (
-              <Match
-                key={match.id}
-                player1Pair1={match.player1Pair1}
-                player2Pair1={match.player2Pair1}
-                player1Pair2={match.player1Pair2}
-                player2Pair2={match.player1Pair2}
-                pointsPair1={match.pointsPair1}
-                pointsPair2={match.pointsPair2}
-              />
-            );
-          })}
+
+        <section className="flex justify-center min-h-[300px] items-center gap-4 md:gap-8">
+          {!existGruops ? (
+            <GenerateGroupsPlayers />
+          ) : (
+            categories.map((category: Category) => {
+              return (
+                <Link
+                  key={category.id}
+                  href={`/dashboard/partidos/${category.id}`}
+                  className="flex flex-col items-center gap-y-1.5 px-4 py-2 border bg-primary text-white font-bold hover:opacity-80 transition-colors"
+                >
+                  Ver Partidos Categoria {category.description}
+                </Link>
+              );
+            })
+          )}
         </section>
       </div>
     </div>
