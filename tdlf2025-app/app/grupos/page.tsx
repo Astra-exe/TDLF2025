2bb4e3ee086bca0414ce908e52b16cf6e43bbb90
francisco-solis99 <franciscosoliscss@gmail.com>
2025-03-22 01:38:45 -0600

    chore: add the structure for groups public page

--- tdlf2025-app/app/grupos/page.tsx
+++ tdlf2025-app/app/grupos/page.tsx
@@ -1,3 +1,9 @@
-export default function page() {
-  return <div>Grupos</div>;
+export default function GroupsPagePublic() {
+  return (
+    <div>
+      <section>
+        <section className="grid grid-cols-2 md:grid-cols-3 gap-5"></section>
+      </section>
+    </div>
+  );
 }
