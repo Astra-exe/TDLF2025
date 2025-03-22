091528ba9d8dbf58ede234fbf0cf07da73f12a92
francisco-solis99 <franciscosoliscss@gmail.com>
2025-03-22 01:33:34 -0600

    feat: add the edit player form and page

---
+++ tdlf2025-app/app/components/EditPlayerForm.tsx
@@ -0,0 +1,239 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/app/components/ui/button";
+import { Input } from "@/app/components/ui/input";
+import { updatePlayerById } from "@/app/lib/actions";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+  FormDescription,
+} from "@/app/components/ui/form";
+import { toast } from "sonner";
+import { Player } from "../lib/definitions";
+
+const inscripcionSchema = z.object({
+  fullname: z
+    .string()
+    .nonempty("Nombre completo es requerido")
+    .min(5, { message: "El nombre debe contener al menos 5 caracteres" })
+    .max(128)
+    .trim(),
+  city: z
+    .string()
+    .nonempty("Ciudad es requerido")
+    .min(1, { message: "La ciudad debe contener al menos 3 caracteres" })
+    .max(128)
+    .trim(),
+  weight: z.coerce
+    .number({
+      required_error: "Peso es requerido",
+    })
+    .positive({ message: "El peso debe ser mayor que 0" })
+    .gte(20, { message: "Debe de ser a partir de 20 o más kg" }),
+  height: z.coerce
+    .number({
+      required_error: "Altura es requerido",
+    })
+    .positive({ message: "La altura debe ser mayor que 0" })
+    .gte(0.5, { message: "Debe de ser a partir de 0.5 mts" })
+    .lte(2.5, { message: "Debe de ser menor de 2.5 mts" }),
+  age: z.coerce
+    .number({
+      required_error: "Edad es requerido",
+    })
+    .int()
+    .positive({ message: "La edad debe ser mayor que 0" })
+    .gte(14, { message: "Debe de ser a partir de los 14 años" }),
+  experience: z.coerce
+    .number({
+      required_error: "Experiencia es requerida",
+    })
+    .positive({ message: "La experiencia debe ser mayor que 0" }),
+});
+
+type InscriptionData = z.infer<typeof inscripcionSchema>;
+
+export default function EditPlayerForm({
+  player,
+}: {
+  player: Partial<Player>;
+}) {
+  console.log(player);
+  const form = useForm<InscriptionData>({
+    resolver: zodResolver(inscripcionSchema),
+    defaultValues: {
+      fullname: player.fullname || "",
+      city: player.city,
+      weight: player.weight,
+      height: player.height,
+      age: player.age,
+      experience: player.experience,
+    },
+  });
+
+  const onSubmit = async (data: InscriptionData) => {
+    console.log(data);
+    try {
+      const result = inscripcionSchema.safeParse(data);
+      if (!result.success) throw new Error("Data invalida");
+      const { fullname, city, weight, height, age, experience } = result.data;
+      toast.promise(
+        updatePlayerById(player.id, {
+          fullname,
+          city,
+          weight,
+          height,
+          age,
+          experience,
+        }),
+        {
+          loading: "Actualizando jugador...",
+          success: () => "Jugador actualizado con éxito!",
+          error: (error) => {
+            if (error instanceof Error) {
+              return error.message.includes("No autorizado")
+                ? "Debes iniciar sesión para crear parejas"
+                : error.message;
+            }
+            return "Error desconocido al actualizar al jugador";
+          },
+        }
+      );
+    } catch (error) {
+      console.error("Unexpected error:", error);
+      toast.error("Error inesperado al actualizar al jugador");
+    }
+  };
+
+  return (
+    <div className="px-6 py-3 sm:p-12 max-w-5xl mx-auto bg-dark/30 rounded-2xl">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+          <FormField
+            control={form.control}
+            name="fullname"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Nombre completo</FormLabel>
+                <FormControl>
+                  <Input
+                    className="ring-1 ring-white/20"
+                    placeholder="Rafael Nadal"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="city"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Ciudad</FormLabel>
+                <FormControl>
+                  <Input
+                    className="ring-1 ring-white/20"
+                    type="text"
+                    placeholder="Irapuato"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="weight"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Peso</FormLabel>
+                <FormControl>
+                  <Input
+                    className="ring-1 ring-white/20"
+                    type="number"
+                    placeholder="72"
+                    {...field}
+                  />
+                </FormControl>
+                <FormDescription>El peso del jugador en kg</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="height"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Altura</FormLabel>
+                <FormControl>
+                  <Input
+                    className="ring-1 ring-white/20"
+                    type="number"
+                    placeholder="1.70"
+                    {...field}
+                  />
+                </FormControl>
+                <FormDescription>La altura del jugador en mts</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="age"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Edad</FormLabel>
+                <FormControl>
+                  <Input
+                    className="ring-1 ring-white/20"
+                    type="number"
+                    placeholder="30"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="experience"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Años de experiencia</FormLabel>
+                <FormControl>
+                  <Input
+                    className="ring-1 ring-white/20"
+                    type="number"
+                    placeholder="3"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button
+            className="cursor-pointer block ml-auto text-white"
+            type="submit"
+            size={"lg"}
+          >
+            Editar jugador
+          </Button>
+        </form>
+      </Form>
+    </div>
+  );
+}
